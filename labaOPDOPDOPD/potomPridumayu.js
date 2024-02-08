0ce2055bf8845387cf67385b7b8ba9b25972679a
Alex <144828762+naku0@users.noreply.github.com>
2024-02-08 22:26:16 +0300

    Add files via upload

--- labaOPDOPDOPD/potomPridumayu.js
+++ labaOPDOPDOPD/potomPridumayu.js
@@ -1,4 +1,4 @@
-function CheckInput(x, y) {
+function CheckInput() {
     let login = document.getElementById("email").value;
     let psw = document.getElementById("psw").value;
     if (login === "creeper2005@opdopdopd.com" && psw === "1488"){
@@ -7,4 +7,15 @@ function CheckInput(x, y) {
         alert("Cука")
     }
 
+}
+function ChangeDiv(id1, id2){
+    id1 = document.getElementById(id1);
+    id2 = document.getElementById(id2);
+    if(id2.style.display === "none"){
+        id1.style.display = "none";
+        id2.style.display = "block";
+    }else{
+        id1.style.display = "block";
+        id2.style.display = "none";
+    }
 }
