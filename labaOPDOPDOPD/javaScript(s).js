980e8d0763b20962e5a2bb99ac5a4c8c892a8c29
Alex <409073@niuitmo.ru>
2024-03-05 22:09:27 +0300

    yooo

--- labaOPDOPDOPD/javaScript(s).js
+++ labaOPDOPDOPD/javaScript(s).js
@@ -1,8 +1,37 @@
+class Flag {
+    static _flag = false;
+
+    static get flag() {
+        return this._flag;
+    }
+
+    static set flag(value) {
+        this._flag = value;
+    }
+}
+
+const flag = new Flag();
+let counter =0;
+
+function CheckFlag(enterId, exitId) {
+    enterId = document.getElementById(enterId);
+    exitId = document.getElementById(exitId);
+    console.log(flag._flag)
+    if (flag._flag === false) {
+        enterId.style.display = "none";
+        exitId.style.display = "flex";
+    } else {
+        enterId.style.display = "flex";
+        exitId.style.display = "none";
+    }
+}
+
 function CheckInput() {
     let login = document.getElementById("email").value;
     let psw = document.getElementById("psw").value;
     if (login === "creeper2005@opdopdopd.com" && psw === "1488") {
-        window.location.href = "OPDOPDOPD.html"
+        window.location.href = "OPDOPDOPD.html";
+        StartSession();
     } else {
         alert("Cука")
     }
@@ -12,9 +41,10 @@ function ShowDiv(class1) {
     class1 = document.getElementById(class1);
     class1.style.display = "grid";
 }
-function closeReg(class1){
-    class1= document.getElementById(class1);
-    class1.style.display ="none";
+
+function closeReg(class1) {
+    class1 = document.getElementById(class1);
+    class1.style.display = "none";
 }
 
 function ChangeDiv(id1, id2) {
@@ -34,7 +64,8 @@ function CheckInputReg() {
     let password = document.getElementById("regPsw").value;
     let passwordConfirm = document.getElementById("psw-repeat").value;
     if (ConfirmPassword(password, passwordConfirm)) {
-        window.location.href = "OPDOPDOPD.html"
+        window.location.href = "OPDOPDOPD.html";
+        StartSession();
     } else {
         alert("Сука")
     }
@@ -44,14 +75,35 @@ function ConfirmPassword(psw1, psw2) {
     return psw1 === psw2;
 }
 
-function EndSession() {
-
+function StartSession() {
+    flag._flag = true;
 }
-function randomLogo(id1){
-    let arr = Array("smoking_rooms", "accessible_forward","mood","functions","favorite",
-        "school","school","school","wifi","wifi","code","");
+
+function EndSession() {
+    flag._flag = false;
+}
+
+function randomLogo(id1) {
+    let arr = Array("smoking_rooms", "accessible_forward", "mood", "functions", "favorite",
+        "school", "school", "wifi", "wifi", "code", "");
     let item = arr[Math.floor(Math.random() * arr.length)];
     console.log(item);
-    let iconElement = document.getElementById(id1);
-    iconElement.textContent = item;
+    document.getElementById(id1).textContent = item;
+}
+function heartClicker(id1,buttonId) {
+
+    id1=document.getElementById(id1);
+    buttonId = document.getElementById(buttonId);
+    function incrementCounter() {
+        counter++;
+        console.log(counter)
+        if ((counter === 2 || counter%3===0) && counter !== 3) {
+            id1.style.display = "flex";
+            id1.scrollIntoView({ behavior: 'smooth', block:'end'});
+        } else{
+            id1.style.display="none";
+        }
+    }
+    buttonId.addEventListener('click', incrementCounter,{once: true});
+
 }
