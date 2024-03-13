50bfdb83cd99061f3ea009543907d987c7b811a8
Kivicol <408547@niuitmo.ru>
2024-03-13 14:16:49 +0300

    db lolololol

--- labaOPDOPDOPD/db_things/registration.js
+++ labaOPDOPDOPD/db_things/registration.js
@@ -6,10 +6,11 @@ const connection = mysql.createConnection({
     password: "qwerty0987654321",
     database: "opdopdopd"
 });
-//функция регистрации
-export function registration(connection, login, password, name, surname){
 
+//функция регистрации
+export function registration(connection, login, password){
     connection.connect(function (err){
+        console.log("БД успешно подняты!");
         if (err) throw err;
         connection.query("SELECT login FROM users", function (err, result, fields){ //запрашиваем все логины
             if (err) throw err;
@@ -22,7 +23,7 @@ export function registration(connection, login, password, name, surname){
             if (!(flag)){ //если есть - шлём нахуй, хотя надо попросить придумать другой логин
                 console.log("User already exist!");
             }else{ //если нет - делаем новую запись в бд и все круто классно
-                connection.query("INSERT INTO users (login, password, name, surname, permissions) VALUES (login, password, name, surname, 0)", function (err, result){
+                connection.query("INSERT INTO users (login, password, permissions) VALUES (login, password, 0)", function (err, result){
                     if (err) throw err;
                     console.log("Registration success!");
                 });
