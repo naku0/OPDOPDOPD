71f8a6ae8a6c22333e1ad60b458a81539915f304
Alex <409073@niuitmo.ru>
2024-04-13 21:28:33 +0300

    test

---
+++ labaOPDOPDOPD/public/script_test1.js
@@ -0,0 +1,84 @@
+let testBtn = document.getElementById('test');
+let startBtn = document.getElementById('start');
+let space = 32;
+let second = 0;
+let milisec = 0;
+let timer = false;
+let counter = 0;
+let amount = 5;
+let results = new Array(amount);
+
+function block_space(btn) {
+    if (btn.keyCode === space) {
+        btn.preventDefault();
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function startTest() {
+    testBtn.style.backgroundColor = "red";
+    timer = true;
+    stopWatch();
+    counter++;
+    console.log("Counter = " + counter);
+}
+
+function restartTest() {
+    timer = false;
+    second = 0;
+    milisec = 0;
+    testBtn.style.backgroundColor = "white";
+    block_space(testBtn);
+}
+
+function isSpaceKeyPressedAndCounterNotZero(event, counter) {
+    return event.keyCode === 32 && counter !== 0;
+}
+
+startBtn.addEventListener('keydown', function (event) {
+    block_space(event);
+    if (isSpaceKeyPressedAndCounterNotZero(event, counter)) {
+        if (counter < amount) {
+            restartTest();
+            const clr = setTimeout(startTest, getRandomInt(1, 5) * 1000);
+        } else {
+            timer = false;
+        }
+    }
+});
+
+startBtn.addEventListener('click', function () {
+    if (counter === 0) {
+        const clr = setTimeout(startTest, getRandomInt(1, 5) * 1000);
+    }
+});
+
+
+function stopWatch() {
+    if (timer) {
+        milisec++;
+
+        if (milisec === 100) {
+            second++;
+            milisec = 0;
+        }
+        let secString = second;
+        let milisecString = milisec;
+
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+
+        if (milisec < 10) {
+            milisecString = "0" + milisecString;
+        }
+
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('milisec').innerHTML = milisecString;
+        setTimeout(stopWatch, 10);
+    }
+}
