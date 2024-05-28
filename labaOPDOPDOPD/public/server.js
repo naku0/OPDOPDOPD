ef1fd41b0ec9b88af1f3ecd4082e55effb71a8c5
EgorovaVarvara <evorovarvara7@gmail.com>
2024-05-28 14:49:53 +0300

    ааааааааа

--- labaOPDOPDOPD/public/server.js
+++ labaOPDOPDOPD/public/server.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection(
         // port: "1337",
         host: "localhost",
         user: "root",
-        password: "qwerty0987654321",
+        password: "1234",
         database: "opdopdopd"
     }
 );
